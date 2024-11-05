234fb24aa4ae244a0d9b9a8f61b3839936433eaf
Alev More <64901448+alevmore@users.noreply.github.com>
2024-11-05 17:35:56 +0200

    Update and rename hw_2_1.py to  SQLite_operations.py

--- hw_2_1/ SQLite_operations.py
+++ hw_2_1/ SQLite_operations.py
@@ -4,11 +4,11 @@ from IPython.core.display_functions import display
 import psycopg2.errors as pe 
 from datetime import datetime
 
-# З'єднання з базою даних SQLite
+# Connection to database SQLite
 conn = sqlite3.connect('data.sqlite')
 cursor = conn.cursor()
 
-# Створення таблиці users
+# Creating table users
 cursor.execute('''
     CREATE TABLE IF NOT EXISTS users (
         id INTEGER PRIMARY KEY AUTOINCREMENT,
@@ -18,7 +18,7 @@ cursor.execute('''
     )
 ''')
 
-# Створення таблиці status
+# Creating table status
 cursor.execute('''
     CREATE TABLE IF NOT EXISTS status (
         id INTEGER PRIMARY KEY AUTOINCREMENT,
@@ -27,7 +27,7 @@ cursor.execute('''
     )
 ''')
 
-# Створення таблиці tasks
+# Creating table tasks
 cursor.execute('''
     CREATE TABLE IF NOT EXISTS tasks (
         id INTEGER PRIMARY KEY AUTOINCREMENT,
@@ -41,38 +41,38 @@ cursor.execute('''
     )
 ''')
 
-# Збереження змін у базі даних
+# Saving changes in database 
 conn.commit()
 
-# Запит на вибір завдань за user-id:
+# Selecting tasks by user_id:
 select_tasks_by_user_id = """
 SELECT *FROM tasks WHERE user_id = ?;
 """
 user_id = 6
 
-# Запит на вибір завдань за статусом (status-id)
+# Selecting tasks by status-id
 select_tasks_by_status = """
 SELECT * FROM tasks WHERE status_id = (SELECT id FROM status WHERE name = ?);
 """
 
 status_name = 'new' 
 
-# Оновлення статусу завдання
+# Updation the task status
 update_task_status = """
 UPDATE tasks
 SET status_id = (SELECT id FROM status WHERE name = ?)
 WHERE id = ?;
 """
 new_status = 'in progress'  # Новий статус для завдання
-task_id = 8  # ID завдання, статус якого потрібно оновити
+task_id = 8  # task ID which should be updated 
 
-# Запит на вибір користувачів без завдань
+# Selecting users without tasks 
 select_users_without_tasks = """
 SELECT id, fullname, email, created_at FROM users
 WHERE id NOT IN (SELECT user_id FROM tasks);
 """
 
-# Приклад даних для нового завдання
+# Example of data for a new task 
 new_task_data = {
     'title': 'Veb-designer',
     'description': 'Creating vebpages',
@@ -80,13 +80,13 @@ new_task_data = {
     'user_id': 1  # ID користувача, для якого додається нове завдання
 }
 
-# Запит INSERT для додавання нового завдання
+# INSERT for adding a new task 
 insert_new_task = """
 INSERT INTO tasks (title, description, status_id, user_id, created_at)
 VALUES (?, ?, ?, ?, ?);
 """
 
-# Параметри для запиту INSERT
+# Parameters for query INSERT
 task_params = (
     new_task_data['title'],
     new_task_data['description'],
@@ -95,44 +95,44 @@ task_params = (
     datetime.now()  # Поточна дата і час для created_at
 )
 
-# Запит SELECT для отримання незавершених завдань
+# SELECT for receiving unfinished tasks 
 select_uncompleted_tasks = """
 SELECT * FROM tasks
 WHERE status_id != (SELECT id FROM status WHERE name = 'completed');
 """
 
-# ID завдання, яке потрібно видалити
+# task ID, which should be deleted
 task_id_to_delete = 1  # Припустимо, що потрібно видалити завдання з ID 1
 
-# Запит DELETE для видалення конкретного завдання за його ID
+# DELETE for deleting a particular task by its ID 
 delete_task_by_id = """
 DELETE FROM tasks
 WHERE id = ?;
 """
 
-# Електронна пошта, за якою проводиться пошук
-search_email = 'erica30@example.net'  # Приклад: пошук всіх користувачів з електронною поштою, що закінчується на example.net
+# Searching users by their email address if it ends on example.net
+search_email = 'erica30@example.net'  
 
-# Запит SELECT з умовою LIKE для фільтрації за електронною поштою
+# SELECT with condition LIKE for filtration by email address
 select_users_by_email = """
 SELECT * FROM users
 WHERE email LIKE ?;
 """
 
-# ID користувача, для якого потрібно оновити ім'я
-user_id_to_update = 1  # Припустимо, що потрібно оновити ім'я користувача з ID 1
+# User ID for updating their name 
+user_id_to_update = 1  
 
-# Нове ім'я користувача
+# New user name
 new_username = 'Alex Korh'
 
-# Запит UPDATE для оновлення імені користувача
+# UPDATE for updating user name 
 update_username = """
 UPDATE users
 SET fullname = ?
 WHERE id = ?;
 """
 
-# Запит SELECT з операторами COUNT та GROUP BY для групування завдань за статусами
+# SELECT with operators COUNT та GROUP BY for grouping tasks by their status 
 select_task_count_by_status = """
 SELECT status.name, COUNT(tasks.id) AS task_count
 FROM tasks
@@ -140,10 +140,10 @@ JOIN status ON tasks.status_id = status.id
 GROUP BY status.name;
 """
 
-# Доменна частина електронної пошти для пошуку
-domain = '%@example.com'  # Приклад: пошук завдань, призначених користувачам з доменом example.com
+# Domain part of the email to search for example.com
+domain = '%@example.com'  
 
-# Запит SELECT з умовою LIKE в поєднанні з JOIN для вибору завдань, призначених користувачам з певною доменною частиною електронної пошти
+# SELECT query with LIKE condition in combination with JOIN to select tasks assigned to users with a specific email domain
 select_tasks_by_email_domain = """
 SELECT tasks.*
 FROM tasks
@@ -151,13 +151,13 @@ JOIN users ON tasks.user_id = users.id
 WHERE users.email LIKE ?;
 """
 
-# Запит SELECT з умовою WHERE для вибору завдань, у яких відсутній опис
+# SELECT query with a WHERE clause to select tasks that do not have a description
 select_tasks_without_description = """
 SELECT * FROM tasks
 WHERE description IS NULL OR description = '';
 """
 
-# Запит SELECT з INNER JOIN для вибору користувачів та їхніх завдань у статусі 'in progress'
+# SELECT query with INNER JOIN to select users and their tasks in the ‘in progress’ status
 select_users_tasks_in_progress = """
 SELECT users.fullname, tasks.title
 FROM users
@@ -166,7 +166,7 @@ INNER JOIN status ON tasks.status_id = status.id
 WHERE status.name = 'in progress';
 """
 
-# Запит SELECT з LEFT JOIN та GROUP BY для вибору користувачів та підрахунку їхніх завдань
+# SELECT query with LEFT JOIN and GROUP BY to select users and count their tasks
 select_users_task_count = """
 SELECT users.fullname, COUNT(tasks.id) AS task_count
 FROM users
@@ -174,7 +174,7 @@ LEFT JOIN tasks ON users.id = tasks.user_id
 GROUP BY users.fullname;
 """
 
-# Execution of creation, fiiling and selectionin tables as well as commiting commands
+# Execution of creation, filing and selection tables as well as committing commands
 with conn:
     if conn is not None:
         try: 
@@ -196,29 +196,29 @@ with conn:
             conn.commit()
             result = cursor.fetchall()
             
-            # Відображення результатів у вигляді DataFrame без уточнення назв колонок
+            # Displaying results in the form of a DataFrame without specifying column names
             if result:
                 df= pd.DataFrame(result, columns=[x[0] for x in cursor.description])
                 display(df)
 
-                print(f"Статус завдання з ID {task_id} успішно оновлено на '{new_status}'.")
-                print("Нове завдання успішно додане!")
-                print(f"Завдання з ID {task_id_to_delete} успішно видалено!")
-                print(f"Ім'я користувача з ID {user_id_to_update} успішно оновлено!")
+                print(f"Task status with ID {task_id} successfully updated to '{new_status}'.")
+                print("New task added successfully!")
+                print(f"Task with ID {task_id_to_delete} successfully deleted!")
+                print(f"User name with ID {user_id_to_update} successfully updated!")
             else: 
-                print("Немає завдань без опису.")
-                print("Немає користувачів з завданнями у статусі 'in progress'.")
-                print("Немає даних для відображення.")
+                print("No tasks without a description.")
+                print("No users with tasks in the status 'in progress'.")
+                print("No data to display.")
         except sqlite3.Error as e:
-            print ("Помилка при виконанні запиту INSERT:", "Помилка при видаленні завдання:", "Помилка при оновленні імені користувача:", e)
-            print("Немає користувачів з завданнями у статусі 'in progress'.")
-            print("Немає даних для відображення.")
+            print ("Error when executing INSERT query:", "Помилка при видаленні завдання:", "Помилка при оновленні імені користувача:", e)
+            print("No users with tasks in the status 'in progress'.")
+            print("No data to display.")
     else: 
         print("Error! cannot create the database connection.")
        
 
-# Закриття з'єднання
+# Close connection
 cursor.close()
 conn.close()
 
-print("Таблиці успішно створено!")
+print("Tables created successfully!")
